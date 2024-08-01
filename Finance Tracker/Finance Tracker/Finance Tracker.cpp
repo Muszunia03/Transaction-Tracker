b615f6c7395260db1e726f6cedbf73a6a3c6bf21
Mikołaj Mach <mach.mikolaj@wp.pl>
2024-08-01 20:47:44 +0200

    start

---
+++ Finance Tracker/Finance Tracker/Finance Tracker.cpp
@@ -0,0 +1,30 @@
+﻿#include <iostream>
+#include <string>
+#include <vector>
+using namespace std;
+
+class Transaction {
+public:
+    std::string date;
+    string type;
+    string category;
+    double amount;
+
+    Transaction(const string& date, const string& type, const string& category, double amount);
+};
+class User {
+    vector <Transaction> Transac;
+    string name;
+
+    User(const string& name);
+    void addTransac(Transaction& Transac);
+};
+class DataBase {
+
+};
+
+int main()
+{
+    std::cout << "Hello World!\n";
+}
+
